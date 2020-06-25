9bb59cf159cf8bfd6050d23b0dd9f6864b16dbf2
qinsen <790459721@qq.com>
2020-06-25 14:34:29 +0800

    pub

---
+++ precache-manifest.12abf8c17a75a59e52743220f6fa1041.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ac3922d45defa4e663148d06848438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8d94118d9ca98a1ca9f",
+    "url": "/static/css/main.fc2163d5.chunk.css"
+  },
+  {
+    "revision": "9331dc3a3468a4706738",
+    "url": "/static/js/2.188dea76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.188dea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d94118d9ca98a1ca9f",
+    "url": "/static/js/main.4e5d3b14.chunk.js"
+  },
+  {
+    "revision": "f2a524a2173710a5164d",
+    "url": "/static/js/runtime-main.96cf4502.js"
+  },
+  {
+    "revision": "21c396672e3e38f881bd4095d960be8b",
+    "url": "/static/media/home_banner_1.21c39667.jpg"
+  },
+  {
+    "revision": "2e5adfded184bf88ba7c76168540817c",
+    "url": "/static/media/home_banner_2.2e5adfde.jpg"
+  }
+]);
